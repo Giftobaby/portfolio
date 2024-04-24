26f85d36a1b214cbca5d96b97736cac66f54b0d0
christo pb <christo@ateamsoftsolutions.com>
2024-04-24 05:30:38 -0400

    Update: Added theme and text styles

---
+++ lib/theme/text_styles.dart
@@ -0,0 +1,469 @@
+// Created by: Christo Pananjickal, Created at: 23-04-2024 12:32 pm
+
+import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:portfolio/theme/theme.dart';
+
+class Ts {
+  static const String _defaultFont = 'Lato';
+
+  static TextStyle ts10W400({Color? color, String? fontFamily}) {
+    return GoogleFonts.getFont(
+      _defaultFont,
+      fontSize: 10,
+      fontWeight: FontWeight.w400,
+      color: color ?? appColors.textDefault,
+    );
+  }
+
+  static TextStyle ts10W500({Color? color, String? fontFamily}) {
+    return GoogleFonts.getFont(
+      fontFamily ?? _defaultFont,
+      fontSize: 10,
+      fontWeight: FontWeight.w500,
+      color: color ?? appColors.textDefault,
+    );
+  }
+
+  static TextStyle ts11W400({Color? color, String? fontFamily}) {
+    return GoogleFonts.getFont(
+      fontFamily ?? _defaultFont,
+      fontSize: 11,
+      fontWeight: FontWeight.w400,
+      color: color ?? appColors.textDefault,
+    );
+  }
+
+  static TextStyle ts11W500({Color? color, String? fontFamily}) {
+    return GoogleFonts.getFont(
+      fontFamily ?? _defaultFont,
+      fontSize: 11,
+      fontWeight: FontWeight.w500,
+      color: color ?? appColors.textDefault,
+    );
+  }
+
+  static TextStyle ts11W800({Color? color, String? fontFamily}) {
+    return GoogleFonts.getFont(
+      fontFamily ?? _defaultFont,
+      fontSize: 11,
+      fontWeight: FontWeight.w800,
+      color: color ?? appColors.textDefault,
+    );
+  }
+
+  static TextStyle ts12W400({Color? color, String? fontFamily}) {
+    return GoogleFonts.getFont(
+      fontFamily ?? _defaultFont,
+      fontSize: 12,
+      fontWeight: FontWeight.w400,
+      color: color ?? appColors.textDefault,
+    );
+  }
+
+  static TextStyle ts12W500({Color? color, String? fontFamily}) {
+    return GoogleFonts.getFont(
+      fontFamily ?? _defaultFont,
+      fontSize: 12,
+      fontWeight: FontWeight.w500,
+      color: color ?? appColors.textDefault,
+    );
+  }
+
+  static TextStyle ts12W600({Color? color, String? fontFamily}) {
+    return GoogleFonts.getFont(
+      fontFamily ?? _defaultFont,
+      fontSize: 12,
+      fontWeight: FontWeight.w600,
+      color: color ?? appColors.textDefault,
+    );
+  }
+
+  static TextStyle ts12W700({Color? color, String? fontFamily}) {
+    return GoogleFonts.getFont(
+      fontFamily ?? _defaultFont,
+      fontSize: 12,
+      fontWeight: FontWeight.w700,
+      color: color ?? appColors.textDefault,
+    );
+  }
+
+  static TextStyle ts13W400({Color? color, String? fontFamily}) {
+    return GoogleFonts.getFont(
+      fontFamily ?? _defaultFont,
+      fontSize: 13,
+      fontWeight: FontWeight.w400,
+      color: color ?? appColors.textDefault,
+    );
+  }
+
+  static TextStyle ts13W500({Color? color, String? fontFamily}) {
+    return GoogleFonts.getFont(
+      fontFamily ?? _defaultFont,
+      fontSize: 13,
+      fontWeight: FontWeight.w500,
+      color: color ?? appColors.textDefault,
+    );
+  }
+
+  static TextStyle ts14W300({Color? color, String? fontFamily}) {
+    return GoogleFonts.getFont(
+      fontFamily ?? _defaultFont,
+      fontSize: 14,
+      fontWeight: FontWeight.w300,
+      color: color ?? appColors.textDefault,
+    );
+  }
+
+  static TextStyle ts14W400({Color? color, String? fontFamily}) {
+    return GoogleFonts.getFont(
+      fontFamily ?? _defaultFont,
+      fontSize: 14,
+      fontWeight: FontWeight.w400,
+      color: color ?? appColors.textDefault,
+    );
+  }
+
+  static TextStyle ts14W500({Color? color, String? fontFamily}) {
+    return GoogleFonts.getFont(
+      fontFamily ?? _defaultFont,
+      fontSize: 14,
+      fontWeight: FontWeight.w500,
+      color: color ?? appColors.textDefault,
+    );
+  }
+
+  static TextStyle ts14W600({Color? color, String? fontFamily}) {
+    return GoogleFonts.getFont(
+      fontFamily ?? _defaultFont,
+      fontSize: 14,
+      fontWeight: FontWeight.w600,
+      color: color ?? appColors.textDefault,
+    );
+  }
+
+  static TextStyle ts14W700({Color? color, String? fontFamily}) {
+    return GoogleFonts.getFont(
+      fontFamily ?? _defaultFont,
+      fontSize: 14,
+      fontWeight: FontWeight.w700,
+      color: color ?? appColors.textDefault,
+    );
+  }
+
+  static TextStyle ts15W400({Color? color, String? fontFamily}) {
+    return GoogleFonts.getFont(
+      fontFamily ?? _defaultFont,
+      fontSize: 15,
+      fontWeight: FontWeight.w400,
+      color: color ?? appColors.textDefault,
+    );
+  }
+
+  static TextStyle ts15W500({Color? color, String? fontFamily}) {
+    return GoogleFonts.getFont(
+      fontFamily ?? _defaultFont,
+      fontSize: 15,
+      fontWeight: FontWeight.w500,
+      color: color ?? appColors.textDefault,
+    );
+  }
+
+  static TextStyle ts15W600({Color? color, String? fontFamily}) {
+    return GoogleFonts.getFont(
+      fontFamily ?? _defaultFont,
+      fontSize: 15,
+      fontWeight: FontWeight.w600,
+      color: color ?? appColors.textDefault,
+    );
+  }
+
+  static TextStyle ts16W400({Color? color, String? fontFamily}) {
+    return GoogleFonts.getFont(
+      fontFamily ?? _defaultFont,
+      fontSize: 16,
+      fontWeight: FontWeight.w400,
+      color: color ?? appColors.textDefault,
+    );
+  }
+
+  static TextStyle ts16W500({Color? color, String? fontFamily}) {
+    return GoogleFonts.getFont(
+      fontFamily ?? _defaultFont,
+      fontSize: 16,
+      fontWeight: FontWeight.w500,
+      color: color ?? appColors.textDefault,
+    );
+  }
+
+  static TextStyle ts16W600({Color? color, String? fontFamily}) {
+    return GoogleFonts.getFont(
+      fontFamily ?? _defaultFont,
+      fontSize: 16,
+      fontWeight: FontWeight.w600,
+      color: color ?? appColors.textDefault,
+    );
+  }
+
+  static TextStyle ts16W700({Color? color, String? fontFamily}) {
+    return GoogleFonts.getFont(
+      fontFamily ?? _defaultFont,
+      fontSize: 16,
+      fontWeight: FontWeight.w700,
+      color: color ?? appColors.textDefault,
+    );
+  }
+
+  static TextStyle ts17W400({Color? color, String? fontFamily}) {
+    return GoogleFonts.getFont(
+      fontFamily ?? _defaultFont,
+      fontSize: 17,
+      fontWeight: FontWeight.w400,
+      color: color ?? appColors.textDefault,
+    );
+  }
+
+  static TextStyle ts17W500({Color? color, String? fontFamily}) {
+    return GoogleFonts.getFont(
+      fontFamily ?? _defaultFont,
+      fontSize: 17,
+      fontWeight: FontWeight.w500,
+      color: color ?? appColors.textDefault,
+    );
+  }
+
+  static TextStyle ts17W600({Color? color, String? fontFamily}) {
+    return GoogleFonts.getFont(
+      fontFamily ?? _defaultFont,
+      fontSize: 17,
+      fontWeight: FontWeight.w600,
+      color: color ?? appColors.textDefault,
+    );
+  }
+
+  static TextStyle ts17W700({Color? color, String? fontFamily}) {
+    return GoogleFonts.getFont(
+      fontFamily ?? _defaultFont,
+      fontSize: 17,
+      fontWeight: FontWeight.w700,
+      color: color ?? appColors.textDefault,
+    );
+  }
+
+  static TextStyle ts18W400({Color? color, String? fontFamily}) {
+    return GoogleFonts.getFont(
+      fontFamily ?? _defaultFont,
+      fontSize: 18,
+      fontWeight: FontWeight.w400,
+      color: color ?? appColors.textDefault,
+    );
+  }
+
+  static TextStyle ts18W500({Color? color, String? fontFamily}) {
+    return GoogleFonts.getFont(
+      fontFamily ?? _defaultFont,
+      fontSize: 18,
+      fontWeight: FontWeight.w500,
+      color: color ?? appColors.textDefault,
+    );
+  }
+
+  static TextStyle ts18W600({Color? color, String? fontFamily}) {
+    return GoogleFonts.getFont(
+      fontFamily ?? _defaultFont,
+      fontSize: 18,
+      fontWeight: FontWeight.w600,
+      color: color ?? appColors.textDefault,
+    );
+  }
+
+  static TextStyle ts18W700({Color? color, String? fontFamily}) {
+    return GoogleFonts.getFont(
+      fontFamily ?? _defaultFont,
+      fontSize: 18,
+      fontWeight: FontWeight.w700,
+      color: color ?? appColors.textDefault,
+    );
+  }
+
+  static TextStyle ts20W400({Color? color, String? fontFamily}) {
+    return GoogleFonts.getFont(
+      fontFamily ?? _defaultFont,
+      fontSize: 20,
+      fontWeight: FontWeight.w400,
+      color: color ?? appColors.textDefault,
+    );
+  }
+
+  static TextStyle ts20W600({Color? color, String? fontFamily}) {
+    return GoogleFonts.getFont(
+      fontFamily ?? _defaultFont,
+      fontSize: 20,
+      fontWeight: FontWeight.w600,
+      color: color ?? appColors.textDefault,
+    );
+  }
+
+  static TextStyle ts20W700({Color? color, String? fontFamily}) {
+    return GoogleFonts.getFont(
+      fontFamily ?? _defaultFont,
+      fontSize: 20,
+      fontWeight: FontWeight.w700,
+      color: color ?? appColors.textDefault,
+    );
+  }
+
+  static TextStyle ts21W400({Color? color, String? fontFamily}) {
+    return GoogleFonts.getFont(
+      fontFamily ?? _defaultFont,
+      fontSize: 21,
+      fontWeight: FontWeight.w400,
+      color: color ?? appColors.textDefault,
+    );
+  }
+
+  static TextStyle ts21W500({Color? color, String? fontFamily}) {
+    return GoogleFonts.getFont(
+      fontFamily ?? _defaultFont,
+      fontSize: 21,
+      fontWeight: FontWeight.w500,
+      color: color ?? appColors.textDefault,
+    );
+  }
+
+  static TextStyle ts22W400({Color? color, String? fontFamily}) {
+    return GoogleFonts.getFont(
+      fontFamily ?? _defaultFont,
+      fontSize: 22,
+      fontWeight: FontWeight.w400,
+      color: color ?? appColors.textDefault,
+    );
+  }
+
+  static TextStyle ts22W700({Color? color, String? fontFamily}) {
+    return GoogleFonts.getFont(
+      fontFamily ?? _defaultFont,
+      fontSize: 22,
+      fontWeight: FontWeight.w700,
+      color: color ?? appColors.textDefault,
+    );
+  }
+
+  static TextStyle ts23W400({Color? color, String? fontFamily}) {
+    return GoogleFonts.getFont(
+      fontFamily ?? _defaultFont,
+      fontSize: 23,
+      fontWeight: FontWeight.w400,
+      color: color ?? appColors.textDefault,
+    );
+  }
+
+  static TextStyle ts23W600({Color? color, String? fontFamily}) {
+    return GoogleFonts.getFont(
+      fontFamily ?? _defaultFont,
+      fontSize: 23,
+      fontWeight: FontWeight.w600,
+      color: color ?? appColors.textDefault,
+    );
+  }
+
+  static TextStyle ts24W400({Color? color, String? fontFamily}) {
+    return GoogleFonts.getFont(
+      fontFamily ?? _defaultFont,
+      fontSize: 24,
+      fontWeight: FontWeight.w400,
+      color: color ?? appColors.textDefault,
+    );
+  }
+
+  static TextStyle ts24W600({Color? color, String? fontFamily}) {
+    return GoogleFonts.getFont(
+      fontFamily ?? _defaultFont,
+      fontSize: 24,
+      fontWeight: FontWeight.w600,
+      color: color ?? appColors.textDefault,
+    );
+  }
+
+  static TextStyle ts24W700({Color? color, String? fontFamily}) {
+    return GoogleFonts.getFont(
+      fontFamily ?? _defaultFont,
+      fontSize: 24,
+      fontWeight: FontWeight.w700,
+      color: color ?? appColors.textDefault,
+    );
+  }
+
+  static TextStyle ts26W400({Color? color, String? fontFamily}) {
+    return GoogleFonts.getFont(
+      fontFamily ?? _defaultFont,
+      fontSize: 26,
+      fontWeight: FontWeight.w400,
+      color: color ?? appColors.textDefault,
+    );
+  }
+
+  static TextStyle ts26W500({Color? color, String? fontFamily}) {
+    return GoogleFonts.getFont(
+      fontFamily ?? _defaultFont,
+      fontSize: 26,
+      fontWeight: FontWeight.w500,
+      color: color ?? appColors.textDefault,
+    );
+  }
+
+  static TextStyle ts26W600({Color? color, String? fontFamily}) {
+    return GoogleFonts.getFont(
+      fontFamily ?? _defaultFont,
+      fontSize: 26,
+      fontWeight: FontWeight.w600,
+      color: color ?? appColors.textDefault,
+    );
+  }
+
+  static TextStyle ts26W700({Color? color, String? fontFamily}) {
+    return GoogleFonts.getFont(
+      fontFamily ?? _defaultFont,
+      fontSize: 26,
+      fontWeight: FontWeight.w700,
+      color: color ?? appColors.textDefault,
+    );
+  }
+
+  static TextStyle ts30W700({Color? color, String? fontFamily}) {
+    return GoogleFonts.getFont(
+      fontFamily ?? _defaultFont,
+      fontSize: 30,
+      fontWeight: FontWeight.w700,
+      color: color ?? appColors.textDefault,
+    );
+  }
+
+  static TextStyle ts40W700({Color? color, String? fontFamily}) {
+    return GoogleFonts.getFont(
+      fontFamily ?? _defaultFont,
+      fontSize: 40,
+      fontWeight: FontWeight.w700,
+      color: color ?? appColors.textDefault,
+    );
+  }
+
+  static TextStyle ts50W500({Color? color, String? fontFamily}) {
+    return GoogleFonts.getFont(
+      fontFamily ?? _defaultFont,
+      fontSize: 50,
+      fontWeight: FontWeight.w500,
+      color: color ?? appColors.textDefault,
+    );
+  }
+
+  static TextStyle ts56W700({Color? color, String? fontFamily}) {
+    return GoogleFonts.getFont(
+      fontFamily ?? _defaultFont,
+      fontSize: 56,
+      fontWeight: FontWeight.w700,
+      color: color ?? appColors.textDefault,
+    );
+  }
+}
---
+++ lib/theme/theme.dart
@@ -0,0 +1,10 @@
+// Created by: Christo Pananjickal, Created at: 23-04-2024 12:33 pm
+// ignore_for_file: library_private_types_in_public_api
+import 'package:flutter/material.dart';
+
+class _AppColors {
+  final Color textDefault;
+  _AppColors({required this.textDefault});
+}
+
+final _AppColors appColors = _AppColors(textDefault: Colors.black);
