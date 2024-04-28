0badef22fd01cb42e5949ad8b25d6c2149a5bdb5
christo pb <christo@ateamsoftsolutions.com>
2024-04-28 17:24:27 -0400

    Update: removed http

--- lib/services/network/user_service.dart
+++ lib/services/network/user_service.dart
@@ -2,7 +2,7 @@
 
 import 'dart:convert';
 import 'package:portfolio/models/api_models/api_response_model.dart';
-import 'package:http/http.dart' as http;
+// import 'package:http/http.dart' as http;
 import 'package:portfolio/models/user_models/user_model.dart';
 import 'package:portfolio/services/network/endpoints.dart';
 
@@ -11,40 +11,41 @@ class UserService {
   /// [getUserInfo] is responsible for fetching the user info from API.
   static Future<ApiResponseModel<UserModel>> getUserInfo() async {
     /// [httpResponse] is the response model of [http.get] method.
-    http.Response? httpResponse;
+    // http.Response? httpResponse;
 
     /// [responseModel] is the response model for [getUserInfo] function.
     ApiResponseModel<UserModel> responseModel = ApiResponseModel<UserModel>(data: null, message: '', success: false);
 
     try {
       /// Get user info through API.
-      httpResponse = await http.get(Endpoints.userInfo);
-
-      /// Http call was successful if the status code starts with '2'.
-      if (httpResponse.statusCode.toString().startsWith('2')) {
-        responseModel.data = _parseUserData(httpResponse.body);
-
-        /// Http call was successful, but parsing user data from json failed.
-        if (responseModel.data == null) {
-          responseModel.success = false;
-          responseModel.message = 'Failed to parse user data.';
-
-          return responseModel;
-        }
-
-        /// Http call was successful and user data parsed successfully.
-        responseModel.success = true;
-        responseModel.message = 'User data fetched successfully';
-
-        return responseModel;
-      }
+      // httpResponse = await http.get(Endpoints.userInfo);
+      //
+      // /// Http call was successful if the status code starts with '2'.
+      // if (httpResponse.statusCode.toString().startsWith('2')) {
+      //   responseModel.data = _parseUserData(httpResponse.body);
+      //
+      //   /// Http call was successful, but parsing user data from json failed.
+      //   if (responseModel.data == null) {
+      //     responseModel.success = false;
+      //     responseModel.message = 'Failed to parse user data.';
+      //
+      //     return responseModel;
+      //   }
+      //
+      //   /// Http call was successful and user data parsed successfully.
+      //   responseModel.success = true;
+      //   responseModel.message = 'User data fetched successfully';
+      //
+      //   return responseModel;
+      // }
 
       /// Status code of http call didn't start with '2', which means the server returned an error.
       throw Exception();
     } catch (_) {
       /// Handle all the errors.
-      String statusCode = httpResponse?.statusCode == null ? '' : ' Status code ${httpResponse!.statusCode}.';
-      responseModel.message = 'Failed to get user info.$statusCode';
+      // String statusCode = httpResponse?.statusCode == null ? '' : ' Status code ${httpResponse!.statusCode}.';
+      // responseModel.message = 'Failed to get user info.$statusCode';
+      responseModel.message = 'Failed to get user info.';
       responseModel.success = false;
       responseModel.data = null;
 
