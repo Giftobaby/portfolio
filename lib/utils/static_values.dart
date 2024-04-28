a6085562d34487ccf216f7c82bd1c2cfceecd0c6
christo pb <christo@ateamsoftsolutions.com>
2024-04-28 12:34:25 -0400

    Update: Added info.json url to static values

--- lib/utils/static_values.dart
+++ lib/utils/static_values.dart
@@ -1,5 +1,5 @@
 // Created by: Christo Pananjickal, Created at: 24-04-2024 06:04 am
 
 class StaticValues {
-  static const String appName = 'Christo\'s Portfolio';
+  static const String infoUrl = 'https://github.com/CHRISTOPANANJICKAL/portfolio/blob/main/files/info.json';
 }
