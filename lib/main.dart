bfbb4c4f90ce8d2d8c300b798c7788ac7f5471cb
christo pb <christo@ateamsoftsolutions.com>
2024-04-28 16:22:13 -0400

    Update: Registered providers for the app

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,11 @@
 import 'package:flutter/material.dart';
+import 'package:portfolio/providers/provider_register.dart';
 import 'package:portfolio/services/navigation_service/app_navigator.dart';
 import 'package:portfolio/theme/theme.dart';
 import 'package:portfolio/utils/static_values.dart';
+import 'package:provider/provider.dart';
 
-void main() => runApp(const MyApp());
+void main() => runApp(MultiProvider(providers: ProviderRegister.providers, child: const MyApp()));
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
