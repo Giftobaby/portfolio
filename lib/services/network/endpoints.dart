0bf6b560f21fcba1f7d12fde0b0fa0a64ca51c9f
christo pb <christo@ateamsoftsolutions.com>
2024-04-28 15:44:10 -0400

    Update: Added endpoints for user info

---
+++ lib/services/network/endpoints.dart
@@ -0,0 +1,7 @@
+// Created by: Christo Pananjickal, Created at: 28-04-2024 12:42 pm
+
+/// [Endpoints] contains all the endpoints in the app.
+class Endpoints {
+  /// [userInfo] endpoint to fetch the user info json.
+  static Uri userInfo = Uri.parse('https://github.com/CHRISTOPANANJICKAL/portfolio/blob/main/files/info.json');
+}
