1a2b94ff779c01d64a0496b60f785757db3b65d9
christo pb <christo@ateamsoftsolutions.com>
2024-04-28 16:20:38 -0400

    Update: Updated code to get user info from provider

--- lib/ui/about_screen/about_screen.dart
+++ lib/ui/about_screen/about_screen.dart
@@ -1,17 +1,27 @@
 // Created by: Christo Pananjickal, Created at: 24-04-2024 04:12 pm
 
 import 'package:flutter/material.dart';
-import 'package:portfolio/theme/text_styles.dart';
+import 'package:portfolio/models/api_models/status_enum.dart';
+import 'package:portfolio/providers/user_info_provider.dart';
+import 'package:portfolio/widgets/custom_progress_indocator.dart';
+import 'package:provider/provider.dart';
 
 class AboutScreen extends StatelessWidget {
   const AboutScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      padding: EdgeInsets.all(190),
-      child:
-          Image.network('https://raw.githubusercontent.com/CHRISTOPANANJICKAL/portfolio/main/files/profile_image.webp'),
+    return Consumer<UserInfoProvider>(
+      builder: (context, provider, child) {
+        return Container(
+          padding: EdgeInsets.all(190),
+          child: provider.status == Status.loading
+              ? const CustomProgressIndicator()
+              : provider.status == Status.success
+                  ? Text(provider.userModel!.about.aboutDescription)
+                  : Text(provider.errorMessage),
+        );
+      },
     );
   }
 }
