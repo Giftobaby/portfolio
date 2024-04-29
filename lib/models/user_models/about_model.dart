1623d6e995445c373b3ea3f7e5e3fcc08b15ba14
christo pb <christo@ateamsoftsolutions.com>
2024-04-29 14:20:56 -0400

    Update: added url model

--- lib/models/user_models/about_model.dart
+++ lib/models/user_models/about_model.dart
@@ -1,15 +1,24 @@
 // Created by: Christo Pananjickal, Created at: 28-04-2024 11:39 am
 
+import 'package:portfolio/models/user_models/url_model.dart';
+
 class AboutModel {
   String greetingMessage;
   String greetingIntro;
   String aboutDescription;
+  List<UrlModel> urls;
 
-  AboutModel({required this.aboutDescription, required this.greetingMessage, required this.greetingIntro});
+  AboutModel({
+    required this.aboutDescription,
+    required this.greetingMessage,
+    required this.greetingIntro,
+    required this.urls,
+  });
 
   factory AboutModel.fromJson(Map<String, dynamic> json) => AboutModel(
         aboutDescription: json['about_description'] ?? '',
         greetingMessage: json['greeting_message'] ?? '',
         greetingIntro: json['greeting_intro'] ?? '',
+        urls: List<UrlModel>.from((json['urls'] ?? []).map((e) => UrlModel.fromJson(e))),
       );
 }
