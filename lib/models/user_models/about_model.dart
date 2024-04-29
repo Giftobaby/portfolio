f52945674807cadcc1740b51190726a194a19142
christo pb <christo@ateamsoftsolutions.com>
2024-04-29 13:06:49 -0400

    Update: Updated about model

--- lib/models/user_models/about_model.dart
+++ lib/models/user_models/about_model.dart
@@ -1,10 +1,15 @@
 // Created by: Christo Pananjickal, Created at: 28-04-2024 11:39 am
 
 class AboutModel {
+  String greetingMessage;
+  String greetingIntro;
   String aboutDescription;
 
-  AboutModel({required this.aboutDescription});
+  AboutModel({required this.aboutDescription, required this.greetingMessage, required this.greetingIntro});
 
-  factory AboutModel.fromJson(Map<String, dynamic> json) =>
-      AboutModel(aboutDescription: json['about_description'] ?? '');
+  factory AboutModel.fromJson(Map<String, dynamic> json) => AboutModel(
+        aboutDescription: json['about_description'] ?? '',
+        greetingMessage: json['greeting_message'] ?? '',
+        greetingIntro: json['greeting_intro'] ?? '',
+      );
 }
