018d06eb16c84a67752d008d37b14c6e762706a9
christo pb <christo@ateamsoftsolutions.com>
2024-04-29 13:07:08 -0400

    Update: Updated about screen

--- lib/ui/about_screen/about_screen.dart
+++ lib/ui/about_screen/about_screen.dart
@@ -1,9 +1,14 @@
 // Created by: Christo Pananjickal, Created at: 24-04-2024 04:12 pm
 
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:portfolio/models/api_models/status_enum.dart';
+import 'package:flutter/widgets.dart';
+import 'package:portfolio/extensions/context_extensions.dart';
 import 'package:portfolio/providers/user_info_provider.dart';
-import 'package:portfolio/widgets/custom_progress_indocator.dart';
+import 'package:portfolio/theme/text_styles.dart';
+import 'package:portfolio/ui/about_screen/widgets/user_image_widget.dart';
+import 'package:portfolio/ui/about_screen/widgets/user_name_widget.dart';
+import 'package:portfolio/widgets/custom_future_builder.dart';
 import 'package:provider/provider.dart';
 
 class AboutScreen extends StatelessWidget {
@@ -13,13 +18,40 @@ class AboutScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return Consumer<UserInfoProvider>(
       builder: (context, provider, child) {
-        return Container(
-          padding: EdgeInsets.all(190),
-          child: provider.status == Status.loading
-              ? const CustomProgressIndicator()
-              : provider.status == Status.success
-                  ? Text(provider.userModel!.about.aboutDescription)
-                  : Text(provider.errorMessage),
+        return CustomFutureBuilder(
+          status: provider.status,
+          onRetryTap: provider.getUserInfo,
+          errorMessage: provider.errorMessage,
+          childBuilder: (context) {
+            return Align(
+              alignment: Alignment.centerLeft,
+              child: Padding(
+                padding: EdgeInsets.only(left: context.percentWidth * 10),
+                child: Column(
+                  mainAxisSize: MainAxisSize.min,
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Row(
+                      crossAxisAlignment: CrossAxisAlignment.end,
+                      children: [
+                        UserImageWidget(userModel: provider.userModel!),
+                        const SizedBox(width: 25),
+                        UserNameWidget(userModel: provider.userModel!),
+                      ],
+                    ),
+                    const SizedBox(height: 80),
+                    SizedBox(
+                      width: context.percentWidth * 55,
+                      child: Text(
+                        provider.userModel!.about.aboutDescription,
+                        style: Ts.ts26W600(),
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            );
+          },
         );
       },
     );
