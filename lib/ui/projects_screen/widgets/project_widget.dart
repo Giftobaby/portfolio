fa1503b64740bbac33cdd495791314fe6b553a97
christo pb <christo@ateamsoftsolutions.com>
2024-05-01 00:13:36 -0400

    Update: Added project_widget.dart

---
+++ lib/ui/projects_screen/widgets/project_widget.dart
@@ -0,0 +1,16 @@
+// Created by: Christo Pananjickal, Created at: 30-04-2024 09:51 pm
+
+import 'package:flutter/material.dart';
+import 'package:portfolio/models/user_models/project_model.dart';
+
+class ProjectWidget extends StatelessWidget {
+  final ProjectModel project;
+  const ProjectWidget({super.key, required this.project});
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [],
+    );
+  }
+}
