8d87ab0527f525ea8b283cc0dcbd53b5735dd3b8
christo pb <christo@ateamsoftsolutions.com>
2024-05-01 12:04:45 -0400

    Update: Added project widget

--- lib/ui/projects_screen/widgets/project_widget.dart
+++ lib/ui/projects_screen/widgets/project_widget.dart
@@ -2,6 +2,11 @@
 
 import 'package:flutter/material.dart';
 import 'package:portfolio/models/user_models/project_model.dart';
+import 'package:portfolio/theme/colors.dart';
+import 'package:portfolio/theme/text_styles.dart';
+import 'package:portfolio/ui/about_screen/widgets/url_list_widget.dart';
+import 'package:portfolio/utils/static_values.dart';
+import 'technologies_used_widget.dart';
 
 class ProjectWidget extends StatelessWidget {
   final ProjectModel project;
@@ -10,7 +15,22 @@ class ProjectWidget extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Column(
-      children: [],
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        Text(project.name, style: Ts.ts20W600(color: appColors.textBlue)),
+        Text(
+          '${StaticValues.commonDateFormat.format(project.fromDate)} - ${project.toDate == null ? 'Current' : StaticValues.commonDateFormat.format(project.toDate!)}',
+          style: Ts.ts16W400(fontStyle: FontStyle.italic, color: project.toDate == null ? appColors.yellow : null),
+        ),
+        const SizedBox(height: 12),
+        Text('Role: ${project.role}', style: Ts.ts17W500(color: appColors.text1)),
+        const SizedBox(height: 2),
+        Text(project.description, style: Ts.ts18W400()),
+        const SizedBox(height: 12),
+        TechnologiesUsed(technologies: project.technologies),
+        if (project.urls.isNotEmpty)
+          Padding(padding: const EdgeInsets.only(top: 12), child: UrlListWidget(urls: project.urls)),
+      ],
     );
   }
 }
