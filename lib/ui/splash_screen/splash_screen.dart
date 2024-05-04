6057a3d99ad82a4dab06ff92c2ad08ab94ff9885
christo pb <christo@ateamsoftsolutions.com>
2024-05-04 09:36:00 -0400

    Update: Updated splash screen

--- lib/ui/splash_screen/splash_screen.dart
+++ lib/ui/splash_screen/splash_screen.dart
@@ -1,10 +1,11 @@
 // Created by: Christo Pananjickal, Created at: 24-04-2024 06:16 am
 
 import 'package:flutter/material.dart';
+import 'package:lottie/lottie.dart';
+import 'package:portfolio/gen/assets.gen.dart';
 import 'package:portfolio/providers/user_info_provider.dart';
 import 'package:portfolio/services/navigation_service/app_navigator.dart';
 import 'package:portfolio/services/navigation_service/routes.dart';
-import 'package:portfolio/theme/text_styles.dart';
 import 'package:portfolio/utils/globals.dart';
 import 'package:provider/provider.dart';
 
@@ -20,22 +21,15 @@ class _SplashScreenState extends State<SplashScreen> {
   void initState() {
     final provider = context.read<UserInfoProvider>();
     WidgetsBinding.instance.addPostFrameCallback((t) async {
-      provider.getUserInfo();
-      await Future.delayed(const Duration(seconds: 1), () {
-        Globals.splashCompleted = true;
-        AppNavigator.navigate(AppRoutes.about);
-      });
+      await Future.wait([provider.getUserInfo(), Future.delayed(const Duration(seconds: 1))]);
+      Globals.splashCompleted = true;
+      AppNavigator.navigate(AppRoutes.about);
     });
     super.initState();
   }
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      child: Text(
-        'Spash Animation',
-        style: Ts.ts16W500(color: Colors.white),
-      ),
-    );
+    return Lottie.asset(Assets.hand);
   }
 }
